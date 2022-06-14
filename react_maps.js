e262892e3e06374f51e881682f7dec8ef7f980c8
Iryna Struk <81155526+strukk311@users.noreply.github.com>
2022-06-14 14:22:35 +0300

    Add files via upload

---
+++ react_maps.js
@@ -0,0 +1,88 @@
+'use strict';
+import React, { useState, useEffect } from "react";
+import {
+  withGoogleMap,
+  withScriptjs,
+  GoogleMap,
+  Marker,
+  InfoWindow
+} from "react-google-maps";
+import * as parkData from "./data/skateboard-parks.json";
+
+const e = React.createElement;
+
+function Map() {
+  const [selectedPark, setSelectedPark] = useState(null);
+
+  useEffect(() => {
+    const listener = e => {
+      if (e.key === "Escape") {
+        setSelectedPark(null);
+      }
+    };
+    window.addEventListener("keydown", listener);
+
+    return () => {
+      window.removeEventListener("keydown", listener);
+    };
+  }, []);
+
+  return (
+    <>
+      <GoogleMap
+      defaultZoom={12}
+      defaultCenter={{ lat: 49.839684, lng: 24.029716 }}
+      >
+      {parkData.features.map(park => (
+        <Marker
+          key={park.properties.PARK_ID}
+          position={{
+            lat: park.geometry.coordinates[1],
+            lng: park.geometry.coordinates[0]
+          }}
+          onClick={() => {
+            setSelectedPark(park);
+          }}
+        />
+      ))}
+
+      {selectedPark && (
+        <InfoWindow
+          onCloseClick={() => {
+            setSelectedPark(null);
+          }}
+          position={{
+            lat: selectedPark.geometry.coordinates[1],
+            lng: selectedPark.geometry.coordinates[0]
+          }}
+        >
+          <div>
+            <h2>{selectedPark.properties.NAME}</h2>
+            <p>{selectedPark.properties.ADDRESS}</p>
+            <p>{selectedPark.properties.HOURS}</p>
+            <p>{selectedPark.properties.PHONE}</p>
+          </div>
+        </InfoWindow>
+      )}
+    </GoogleMap>
+    </>
+  );
+}
+
+const MapWrapped = withScriptjs(withGoogleMap(Map));
+
+export default function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <MapWrapped
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDR8pw3EJAalemBIHlTcGYOcFk0enwWO4E`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    </div>
+  );
+}
+
+const domContainer = document.querySelector('#map_container');
+ReactDOM.render(e(App), domContainer);
